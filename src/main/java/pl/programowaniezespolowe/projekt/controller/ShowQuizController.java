3c8d4fbbddd2fd400d64a49bd1c9b299632f4ace
aniauli <ania.uli10@gmail.com>
2021-02-18 15:21:44 +0100

    Nothing, because of @ModelAtribute. Still Learning

--- src/main/java/pl/programowaniezespolowe/projekt/controller/ShowQuizController.java
+++ src/main/java/pl/programowaniezespolowe/projekt/controller/ShowQuizController.java
@@ -1,18 +1,9 @@
 package pl.programowaniezespolowe.projekt.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import pl.programowaniezespolowe.projekt.model.Question;
 import pl.programowaniezespolowe.projekt.model.Quiz;
-import pl.programowaniezespolowe.projekt.repository.AnswerRepository;
-import pl.programowaniezespolowe.projekt.repository.QuestionRepository;
-import pl.programowaniezespolowe.projekt.service.AnswerServiceImpl;
-import pl.programowaniezespolowe.projekt.service.QuestionServiceImpl;
-import pl.programowaniezespolowe.projekt.service.QuizServiceImpl;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
