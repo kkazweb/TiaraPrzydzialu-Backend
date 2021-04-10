203bf2b81b1feba2fc2d6df6480a40112205d3b8
Kacper Kaźmierczak <kkaz2611@gmail.com>
2021-04-10 02:58:28 +0200

    Password changing is done.

--- src/main/java/pl/programowaniezespolowe/projekt/controller/CredentialsController.java
+++ src/main/java/pl/programowaniezespolowe/projekt/controller/CredentialsController.java
@@ -1,4 +1,25 @@
 package pl.programowaniezespolowe.projekt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import pl.programowaniezespolowe.projekt.payload.request.ChangePasswordRequest;
+import pl.programowaniezespolowe.projekt.service.CredentialsService;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/credentials")
 public class CredentialsController {
+
+    @Autowired
+    private CredentialsService credentialsService;
+
+
+    @PostMapping("/changepassword")
+    public String changePassword(@RequestBody ChangePasswordRequest passwordRequest) throws Exception {
+        if(this.credentialsService.changePassword(passwordRequest.getUserId(), passwordRequest.getOldPassword(), passwordRequest.getNewPassword())){
+            return "Hasło zmienione pomyślnie.";
+        }
+        return "Nie zmieniono hasła.";
+    }
 }
--- src/main/java/pl/programowaniezespolowe/projekt/service/CredentialsService.java
+++ src/main/java/pl/programowaniezespolowe/projekt/service/CredentialsService.java
@@ -2,7 +2,10 @@ package pl.programowaniezespolowe.projekt.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.programowaniezespolowe.projekt.model.User;
 import pl.programowaniezespolowe.projekt.payload.request.LoginRequest;
 import pl.programowaniezespolowe.projekt.repository.UserRepository;
 
@@ -13,10 +16,21 @@ public class CredentialsService {
     private LoginService loginService;
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private UserRepository userRepository;
 
-    private void changePassword(String password){
-
+    public boolean changePassword(Long userId, String passwordOld, String passwordNew) throws Exception {
+        User user1 = userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("User with ID: " + userId + " not found - CredentialsService"));
+        if(passwordEncoder.matches(passwordOld, user1.getPassword())){
+            user1.setPassword(passwordEncoder.encode(passwordNew));
+            userRepository.save(user1);
+            return true;
+        }
+        else{
+            throw new Exception("Passwords do not match.");
+        }
     }
 
 }
