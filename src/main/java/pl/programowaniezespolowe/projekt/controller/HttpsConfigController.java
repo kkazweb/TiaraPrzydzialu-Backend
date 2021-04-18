4c6ac169b601327a60b8c76624272e172e2d2411
Kacper Kaźmierczak <kkaz2611@gmail.com>
2021-04-18 18:13:02 +0200

    Controller for https config

---
+++ src/main/java/pl/programowaniezespolowe/projekt/controller/HttpsConfigController.java
@@ -0,0 +1,15 @@
+package pl.programowaniezespolowe.projekt.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class HttpsConfigController {
+
+    @RequestMapping("/.well-known/acme-challenge/hk4FOIDN6GUqSGX5yeuM6Qvg2U_hh61Lp_t_H5ihAB4")
+    public String longStringForConfig(){
+        return "hk4FOIDN6GUqSGX5yeuM6Qvg2U_hh61Lp_t_H5ihAB4.02MM5ctQ5FjNuROhIB2v0n14vjvG9Xq9KYK0k40-APw";
+    }
+}
