830fa2a950d20fdbb8cbc22e2d149f0f493a3349
Kacper Kaźmierczak <kkaz2611@gmail.com>
2021-04-18 18:20:49 +0200

    18.04 trying 2

--- src/main/java/pl/programowaniezespolowe/projekt/controller/HttpsConfigController.java
+++ src/main/java/pl/programowaniezespolowe/projekt/controller/HttpsConfigController.java
@@ -1,6 +1,7 @@
 package pl.programowaniezespolowe.projekt.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HttpsConfigController {
 
-    @RequestMapping("/.well-known/acme-challenge/hk4FOIDN6GUqSGX5yeuM6Qvg2U_hh61Lp_t_H5ihAB4")
+    @GetMapping("/.well-known/acme-challenge/hk4FOIDN6GUqSGX5yeuM6Qvg2U_hh61Lp_t_H5ihAB4")
     public String longStringForConfig(){
         return "hk4FOIDN6GUqSGX5yeuM6Qvg2U_hh61Lp_t_H5ihAB4.02MM5ctQ5FjNuROhIB2v0n14vjvG9Xq9KYK0k40-APw";
     }
